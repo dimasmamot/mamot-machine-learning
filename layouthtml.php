e4e02971fd6e536fa99692622d279dbd408d6c2e
Mamot <dimas.rizky.hp@gmail.com>
2017-05-15 09:47:31 +0700

    Initial Commit

---
+++ layouthtml.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- REQUIRED METAS -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- CSS BOOTSTRAP -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/custom.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Hello World!!</h1>
+  </div>
+  <!-- FILE JQUERY, TETHER, JS BOOTSTRAP -->
+  <script src="js/jquery-3.1.1.min.js"></script>
+  <script src="js/tether-1.4.0.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
